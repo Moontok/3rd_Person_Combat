b15658d1c92b48bd425b409f706dad7e0bcb6ac8
Zackary <zspink@nea.k12.ar.us>
2022-08-26 13:12:40 -0500

    Added Input Events

--- Unity_3rd_Person/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Unity_3rd_Person/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [field: SerializeField] public InputReader InputReader { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SwitchState(new PlayerTestState(this));
     }
 }
---
+++ Unity_3rd_Person/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    private float timer = 0f;
+
+    public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine){}
+
+    public override void Enter()
+    {
+        stateMachine.InputReader.JumpEvent += OnJump;
+    }
+
+    public override void Tick(float deltaTime)
+    {        
+        timer += deltaTime;
+        Debug.Log(timer);
+    }
+
+    public override void Exit()
+    {
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
+    }
+}
