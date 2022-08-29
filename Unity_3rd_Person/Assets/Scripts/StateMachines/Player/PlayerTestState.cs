87f51e79f816ae994b6f5265a6333f0c8a680d0f
Zackary <zspink@nea.k12.ar.us>
2022-08-29 12:53:44 -0500

    Camera Relative Movement 20

--- Unity_3rd_Person/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Unity_3rd_Person/Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -10,10 +10,7 @@ public class PlayerTestState : PlayerBaseState
 
     public override void Tick(float deltaTime)
     {
-        Vector3 movement = new Vector3();
-        movement.x = stateMachine.InputReader.MovementValue.x;
-        movement.z = stateMachine.InputReader.MovementValue.y;
-        movement.y = 0;
+        Vector3 movement = CalculateMovement();
 
         stateMachine.Controller.Move(movement * deltaTime * stateMachine.FreeLookMovementSpeed);
 
@@ -30,5 +27,19 @@ public class PlayerTestState : PlayerBaseState
 
     public override void Exit()
     {
+    }
+
+    private Vector3 CalculateMovement()
+    {
+        Vector3 forward = stateMachine.MainCameraTransform.forward;
+        Vector3 right = stateMachine.MainCameraTransform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        return forward * stateMachine.InputReader.MovementValue.y + right * stateMachine.InputReader.MovementValue.x;
     }
 }
