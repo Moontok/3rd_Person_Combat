46adf8a22144c6533ae5abfbc820085a4e53fbca
Zackary <zspink@nea.k12.ar.us>
2022-08-31 11:36:41 -0500

    Improved Targeting Selection 33

--- Unity_3rd_Person/Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Unity_3rd_Person/Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -8,7 +8,13 @@ public class Targeter : MonoBehaviour
 
     [SerializeField] private CinemachineTargetGroup cineTargetGroup = null;
 
+    private Camera mainCamera = null;
     private List<Target> targets = new List<Target>();
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -29,7 +35,30 @@ public class Targeter : MonoBehaviour
     {
         if (targets.Count == 0) return false;
 
-        CurrentTarget = targets[0];
+        Target closestTarget = null;
+        float closestTargetDistance = Mathf.Infinity;
+
+        foreach (Target target in targets)
+        {
+            Vector2 viewPos = mainCamera.WorldToViewportPoint(target.transform.position);
+
+            if (viewPos.x < 0 || viewPos.x > 1 || viewPos.y < 0 || viewPos.y > 1)
+            {
+                continue;
+            }
+
+            Vector2 toCenter = viewPos - new Vector2(0.5f, 0.5f);
+            if (toCenter.sqrMagnitude < closestTargetDistance)
+            {
+                closestTarget = target;
+                closestTargetDistance = toCenter.sqrMagnitude;
+            }
+        }
+
+        CurrentTarget = closestTarget;
+
+        if (CurrentTarget == null) return false;
+
         cineTargetGroup.AddMember(CurrentTarget.transform, 1, 2f);
 
         return true;
