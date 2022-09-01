351bfe61002ab7ace1c965a7230c96d4825d4880
Zackary <zspink@nea.k12.ar.us>
2022-09-01 10:59:39 -0500

    Enemy Idle State 45

---
+++ Unity_3rd_Person/Assets/Scripts/StateMachines/Enemy/EnemyIdleState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    private readonly int MovementHash = Animator.StringToHash("Movement");
+    private readonly int SpeedHash = Animator.StringToHash("Speed");
+
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine){}
+
+    private const float CrossFadeDuration = 0.1f;
+    private const float AnimatorDampTime = 0.1f;
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(MovementHash, CrossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        stateMachine.Animator.SetFloat(SpeedHash, 0, AnimatorDampTime, deltaTime);
+    }
+
+    public override void Exit(){}
+}
